46cdba113a2d6153fbc3ede4f8996e917131fcb4
GreeNox <udevs@udevs.io>
2025-07-29 17:01:48 +0500

    Woof App Completed

--- app/src/main/java/dev/greenox/MainActivity.kt
+++ app/src/main/java/dev/greenox/MainActivity.kt
@@ -4,51 +4,62 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import dev.greenox.data.Datasource
-import dev.greenox.model.Courses
-import dev.greenox.ui.theme.CoursesAppTheme
+import dev.greenox.data.Dogs
+import dev.greenox.model.Dog
+import dev.greenox.ui.theme.WoofAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CoursesAppTheme {
+            WoofAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    CoursesApp()
+                    WoofApp()
                 }
             }
         }
@@ -56,81 +67,189 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CourseGridItem(
-    course: Courses,
-    modifier: Modifier = Modifier,
+fun WoofApp() {
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(items = Dogs().dogs) {
+                DogItem(
+                    dog = it,
+                    modifier = Modifier
+                        .padding(all = dimensionResource(id = R.dimen.padding_8))
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DogItem(
+    dog: Dog,
+    modifier: Modifier = Modifier
 ) {
-    Box (
-        modifier = modifier
-            .padding(all = 4.dp)
-            .clip(shape = RoundedCornerShape(size = 16.dp))
-            .background(color = colorResource(id = R.color.lavender_mist))
-            .fillMaxWidth()
+    var expanded by remember { mutableStateOf(value = false) }
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.inversePrimary,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        )
     ) {
-        Row {
-            Image(
-                contentDescription = stringResource(id = course.name),
-                painter = painterResource(id = course.image),
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
+            Row(
                 modifier = Modifier
-                    .size(size = 68.dp)
-                    .aspectRatio(ratio = 1F)
-            )
-            Column(
-                modifier = Modifier
-                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                    .fillMaxWidth()
+                    .padding(all = dimensionResource(id = R.dimen.padding_8))
             ) {
+                DogIcon(dogIcon = dog.image)
+                DogInformation(dogName = dog.name, dogAge = dog.age)
+                Spacer(Modifier.weight(weight = 1f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded },
+                )
+            }
+            if (expanded) {
+                DogHobby(
+                    dogHobby = dog.hobbies,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_16),
+                        top = dimensionResource(id = R.dimen.padding_8),
+                        bottom = dimensionResource(id = R.dimen.padding_16),
+                        end = dimensionResource(id = R.dimen.padding_16)
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+    ) {
+        Icon(
+            imageVector =
+                if (expanded)
+                    Icons.Filled.KeyboardArrowUp
+                else
+                    Icons.Filled.KeyboardArrowDown,
+            contentDescription = stringResource(id = R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(size = dimensionResource(id = R.dimen.image_size))
+                        .padding(all = dimensionResource(id = R.dimen.padding_8)),
+                    painter = painterResource(id = R.drawable.ic_woof_logo),
+                    contentDescription = null
+                )
                 Text(
-                    text = stringResource(id = course.name),
-                    style = MaterialTheme.typography.bodyMedium
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
                 )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(top = 8.dp)
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_grain),
-                        contentDescription = null
-                    )
-                    Text(
-                        text = course.size.toString(),
-                        modifier = Modifier.padding(start = 8.dp),
-                        style = MaterialTheme.typography.labelMedium
-                    )
-                }
             }
-        }
-    }
+        },
+    )
 }
 
 @Composable
-fun CoursesApp() {
-    Surface(
+fun DogIcon(
+    @DrawableRes dogIcon: Int,
+) {
+    Image(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 4.dp)
-            .padding(paddingValues = WindowInsets.safeDrawing.asPaddingValues())
-    ) {
-        LazyVerticalGrid (
-            columns = GridCells.Fixed(count = 2),
+            .size(size = dimensionResource(id = R.dimen.image_size))
+            .padding(all = dimensionResource(id = R.dimen.padding_8))
+            .clip(shape = MaterialTheme.shapes.extraLarge),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(id = dogIcon),
+        contentDescription = null
+    )
+}
 
-        ){
-            items(
-                items = Datasource().topics
-            ) {
-                course -> CourseGridItem(
-                    course = course
-                )
-            }
-        }
+@Composable
+fun DogInformation(
+    @StringRes dogName: Int,
+    dogAge: Int,
+) {
+    Column(
+        modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_8))
+    ) {
+        Text(
+            text = stringResource(id = dogName),
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.years_old, dogAge),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_4))
+        )
     }
 }
 
-@Preview(
-    showSystemUi = true,
-    showBackground = true,
-    name = "Courses app"
-)
 @Composable
-fun CoursesPreview() {
-    CoursesApp()
+fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = stringResource(id = dogHobby),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(
+                top = dimensionResource(id = R.dimen.padding_4)
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun WoofPreview() {
+    WoofAppTheme(darkTheme = false) {
+        WoofApp()
+    }
+}
+
+@Preview
+@Composable
+fun WoofDarkThemePreview() {
+    WoofAppTheme(darkTheme = true) {
+        WoofApp()
+    }
 }
--- app/src/main/java/dev/greenox/ui/theme/Type.kt
+++ app/src/main/java/dev/greenox/ui/theme/Type.kt
@@ -2,33 +2,178 @@ package dev.greenox.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.greenox.R
 
 // Set of Material typography styles to start with
+
+val NewsReader = FontFamily(
+    Font(resId = R.font.newsreader_9pt_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.newsreader_14pt_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.newsreader_24pt_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.newsreader_36pt_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.newsreader_60pt_extra_light, weight = FontWeight.ExtraLight),
+    Font(resId = R.font.newsreader_9pt_light, weight = FontWeight.Light),
+    Font(resId = R.font.newsreader_14pt_light, weight = FontWeight.Light),
+    Font(resId = R.font.newsreader_24pt_light, weight = FontWeight.Light),
+    Font(resId = R.font.newsreader_36pt_light, weight = FontWeight.Light),
+    Font(resId = R.font.newsreader_60pt_light, weight = FontWeight.Light),
+    Font(resId = R.font.newsreader_9pt_regular),
+    Font(resId = R.font.newsreader_14pt_regular),
+    Font(resId = R.font.newsreader_24pt_regular),
+    Font(resId = R.font.newsreader_36pt_regular),
+    Font(resId = R.font.newsreader_60pt_regular),
+    Font(resId = R.font.newsreader_9pt_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.newsreader_14pt_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.newsreader_24pt_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.newsreader_36pt_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.newsreader_60pt_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.newsreader_9pt_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.newsreader_14pt_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.newsreader_24pt_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.newsreader_36pt_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.newsreader_60pt_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.newsreader_9pt_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.newsreader_14pt_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.newsreader_24pt_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.newsreader_36pt_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.newsreader_60pt_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.newsreader_9pt_extra_bold, weight = FontWeight.ExtraBold),
+    Font(resId = R.font.newsreader_14pt_extra_bold, weight = FontWeight.ExtraBold),
+    Font(resId = R.font.newsreader_24pt_extra_bold, weight = FontWeight.ExtraBold),
+    Font(resId = R.font.newsreader_36pt_extra_bold, weight = FontWeight.ExtraBold),
+    Font(resId = R.font.newsreader_60pt_extra_bold, weight = FontWeight.ExtraBold),
+    Font(resId = R.font.newsreader_9pt_extra_light_italic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_extra_light_italic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_extra_light_italic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_extra_light_italic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_extra_light_italic, weight = FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_light_italic, weight = FontWeight.Light, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_italic, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_italic, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_italic, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_italic, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_italic, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_semi_bold_italic, weight = FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_bold_italic, weight = FontWeight.Bold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_9pt_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_14pt_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_24pt_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_36pt_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(resId = R.font.newsreader_60pt_extra_bold_italic, weight = FontWeight.ExtraBold, style = FontStyle.Italic),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = NewsReader,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NewsReader,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 28.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = NewsReader,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NewsReader,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 16.sp
     )
-    */
 )
